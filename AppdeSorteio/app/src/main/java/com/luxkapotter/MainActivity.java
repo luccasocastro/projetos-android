2266bc0a4101bf90a36bcd50e741fe11c308c34c
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-02-03 20:52:38 -0300

    criando o primeiro app de sorteio

---
+++ AppdeSorteio/app/src/main/java/com/luxkapotter/MainActivity.java
@@ -0,0 +1,28 @@
+package com.luxkapotter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void jogar(View view) {
+        TextView textResultado;
+
+        textResultado = findViewById(R.id.textResultado);
+
+        int numero = new Random().nextInt(11);
+
+        textResultado.setText("Número sorteado: " + numero);
+    }
+}
